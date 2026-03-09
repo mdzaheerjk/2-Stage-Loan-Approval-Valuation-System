5c971123ac270b47f4eaa4e19a89f47a9f55d30a
zaheer <info.zaheerjk@gmail.com>
2026-03-10 04:06:49 +0530

    predict

--- 2-Stage-Loan-Approval-Valuation-System/code/app/predict.py
+++ 2-Stage-Loan-Approval-Valuation-System/code/app/predict.py
@@ -3,3 +3,22 @@ def two_stage_predict(cls,reg,appliacnat_df):
     preds=cls.predict(appliacnat_df)
 
     results=[]
+    i=0
+    approved_idx=1
+
+    approved=int(preds[i])
+    approved_prob=float(proba[1,approved_idx])
+
+
+    reg_pred=None
+    if approved==1:
+        reg_appliacnat_df=appliacnat_df.copy()
+        reg_appliacnat_df['loan_status']='Approve'
+        reg_pred=float(reg.predict(reg_appliacnat_df)[0])
+    results.append({
+        'approved':approved,
+        'approved_prob':approved_prob,
+        'reg_pred':reg_pred
+    })
+
+    return results
